d4d57f35fe1ec26e1e4ee6873a23b3f536157ce5
Arsenii Kadaner <arseniikadaner@gmail.com>
2020-12-23 10:23:15 +0300

    better rounding

--- utils_ak/numeric/numeric.py
+++ utils_ak/numeric/numeric.py
@@ -1,10 +1,8 @@
 """ Math functionality. """
-import math
-import numpy as np
 from decimal import ROUND_HALF_DOWN, ROUND_DOWN, ROUND_CEILING, ROUND_05UP, ROUND_FLOOR, ROUND_HALF_EVEN, ROUND_HALF_UP, ROUND_UP, Decimal as D
 
-ROUND_DIC = {'round': ROUND_HALF_UP,
-             'nearest': ROUND_HALF_UP,
+ROUND_DIC = {'round': ROUND_HALF_EVEN,
+             'nearest': ROUND_HALF_EVEN,
              'floor': ROUND_FLOOR,
              'ceil': ROUND_CEILING,
              'down': ROUND_DOWN,
@@ -12,35 +10,7 @@ ROUND_DIC = {'round': ROUND_HALF_UP,
              'up': ROUND_UP}
 
 
-# be careful to use this for precise values: rounding 5. to 1. with floor method may result in 4 due to python floating system (5. == 4.99999999999) in python and stuff
-def custom_round(a, b, rounding='round', precision=0):
-    """
-    Precision is used only on
-    """
-    # methods=['round', 'floor', 'ceil', 'down'/'zero', 'up']. half is handled randomly because of python rounding issues
-    n = a / b / 0.1 ** precision
-
-    rounding = ROUND_DIC.get(rounding, rounding)
-
-    if rounding in [ROUND_HALF_DOWN, ROUND_HALF_UP, ROUND_HALF_EVEN]:
-        n = int(round(n))
-    elif rounding == ROUND_FLOOR:
-        n = int(math.floor(n))
-    elif rounding == ROUND_CEILING:
-        n = int(math.ceil(n))
-    elif rounding == ROUND_DOWN:
-        n = int(math.floor(abs(n))) * np.sign(n)
-    elif rounding in ROUND_UP:
-        n = int(math.ceil(abs(n))) * np.sign(n)
-    else:
-        raise Exception('Unknown rounding method')
-    return n * b * 0.1 ** precision
-
-
-def decimal_round(a, b, rounding=ROUND_HALF_DOWN, precision=0, strip=False):
-    """
-    :param rounding: one of [ROUND_CEILING, ROUND_FLOOR, ROUND_UP, ROUND_DOWN, ROUND_HALF_UP, ROUND_HALF_DOWN, ROUND_HALF_EVEN, ROUND_05UP]
-    """
+def decimal_round(a, b, rounding='round', precision=0, strip=False):
     a, b = D(a), D(b)
     n = a / b / D('.1') ** precision
     rounding = ROUND_DIC.get(rounding, rounding)
@@ -49,6 +19,13 @@ def decimal_round(a, b, rounding=ROUND_HALF_DOWN, precision=0, strip=False):
     if strip:
         res = strip_decimal(res)
     return res
+
+
+# be careful to use this for precise values: rounding 5. to 1. with floor method may result in 4 due to python floating system (5. == 4.99999999999) in python and stuff
+def custom_round(a, b, rounding='round', precision=0):
+    n = a / b / 0.1 ** precision
+    n = int(decimal_round(n, 1, rounding))
+    return n * b * 0.1 ** precision
 
 
 def cast_decimal(f, precision=15, strip=True):
@@ -69,14 +46,12 @@ def strip_zeros(s):
 
 
 if __name__ == '__main__':
-    print(custom_round(2.01, 0.1))
-    print(custom_round(2.01, 0.01, precision=1))
-    print(custom_round(2.01, 0.01))
-    print(custom_round(2.01, 0.001))
+    roundings = [ROUND_HALF_DOWN, ROUND_DOWN, ROUND_CEILING, ROUND_05UP, ROUND_FLOOR, ROUND_HALF_EVEN, ROUND_HALF_UP, ROUND_UP]
+    roundings += list(ROUND_DIC.keys())
 
-    print(custom_round(1.123, 2))
-    print(custom_round(-1.123, 1, 'down'))
-    print('{:.12f}'.format(0.1))
+    for rounding in roundings:
+        for v in [1.99, 1.05, -1.99, -1.05]:
+            print(rounding, v, 0.1, custom_round(v, 0.1, rounding))
 
     # 1.99
     print(decimal_round('1.99', '0.01', rounding='ROUND_FLOOR'))
