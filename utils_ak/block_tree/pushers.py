418a3d02333252312c9cefcfe48f3909d1739445
Arsenii Kadaner <arseniikadaner@gmail.com>
2020-12-26 16:42:19 +0300

    stack_push

--- utils_ak/block_tree/pushers.py
+++ utils_ak/block_tree/pushers.py
@@ -1,3 +1,4 @@
+import numpy as np
 from app.schedule_maker.utils.interval import calc_interval_length, cast_interval
 from utils_ak.serialization import cast_js
 from utils_ak import cast_dict_or_list
@@ -5,11 +6,14 @@ from utils_ak import cast_dict_or_list
 
 def validate_disjoint_by_axis(b1, b2, axis=0):
     try:
-        disposition = b1.y[axis] - b2.x[axis]
+        disposition = int(b1.y[axis] - b2.x[axis])
     except:
         disposition = 1
 
-    assert calc_interval_length(b1.interval(axis) & b2.interval(axis)) == 0, cast_js({'disposition': disposition})
+    i1 = cast_interval(b1.x[axis], b2.y[axis])
+    i2 = cast_interval(b2.x[axis], b2.y[axis])
+
+    assert calc_interval_length(i1 & i2) == 0, cast_js({'disposition': disposition})
 
 # def gen_pair_validator(validate):
 #     def f(parent, new_block):
@@ -35,13 +39,18 @@ def simple_push(parent, block, validator=None, new_props=None):
                 return cast_dict_or_list(e.__str__())  # {'disposition': 2}
             except:
                 return
-    return parent.add(block)
+    return parent.add_child(block)
 
 
 def add_push(parent, block, new_props=None):
     return simple_push(parent, block, validator=None, new_props=new_props)
 
-#
+
+def stack_push(parent, block, axis=0):
+    cur_end = 0 if not parent.children else max(c.y[axis] - parent.x[axis] for c in parent.children)
+    block.props.relative_props.setdefault('x', np.zeros(block.n_dims))[axis] = cur_end
+    return add_push(parent, block)
+
 # # simple greedy algorithm
 # def dummy_push(parent, block, max_tries=24, beg='last_end', end=PERIODS_PER_DAY * 10, axis='x', validator='x', iter_props=None):
 #     # note: make sure parent abs props are updated
@@ -89,3 +98,34 @@ def add_push(parent, block, new_props=None):
 #     raise Exception('Failed to push element')
 
 if __name__ == '__main__':
+    from utils_ak.block_tree import IntParallelepipedBlock
+
+    a = IntParallelepipedBlock('a', n_dims=2, x=np.array([1, 0]), size=[4, 0])
+    b = IntParallelepipedBlock('b', n_dims=2, x=np.array([3, 0]), size=[3, 0])
+    try:
+        validate_disjoint_by_axis(a, b, 0)
+    except AssertionError as e:
+        print('AssertionError on disposition', e)
+
+    root = IntParallelepipedBlock('root', n_dims=1, x=np.array([2]))
+    a = IntParallelepipedBlock('a', n_dims=1, size=[4])
+    b = IntParallelepipedBlock('b', n_dims=1, size=[3])
+    stack_push(root, a)
+    stack_push(root, b)
+    print(root)
+
+    #
+    #
+    # c = IntParallelepipedBlock('c', n_dims=2, x=np.array([3, 4]), size=np.array([1, 5]))
+    # a.add_child(b)
+    # b.add_child(c)
+    #
+    # print(a)
+    # print(b)
+    # print(c)
+    # print(a.x, a.size, a.y)
+    # print(b.x, b.size, b.y)
+    # print(c.x, c.size, c.y)
+    #
+    # print()
+    # print(a['b']['c'])
