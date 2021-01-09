3ccf06fad64f1b0dfead3e71d7dbeca2906f7b43
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-01-09 21:57:08 +0300

    hotfix

--- utils_ak/fluid_flow/actors/stub.py
+++ utils_ak/fluid_flow/actors/stub.py
@@ -5,5 +5,11 @@ class Stub(Actor):
     def __init__(self, id=None):
         super().__init__(id)
 
+    def update_pressure(self, ts):
+        for pipe in self.parents:
+            pipe.pressure_out = 0
+        for pipe in self.children:
+            pipe.pressure_in = 0
+
     def __str__(self):
         return f'Stub {self.id}'
