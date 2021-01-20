bf3825389043d120c549bd459d90768c1ea94189
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-01-20 11:14:19 +0300

    refactoring

--- utils_ak/mongoengine/mongoengine_tools.py
+++ utils_ak/mongoengine/mongoengine_tools.py
@@ -16,26 +16,26 @@ def cast_object_id(obj):
         raise Exception('Unknown object id type')
 
 
-def cast_model(fs_obj, cls):
-    if isinstance(fs_obj, ObjectId):
-        return cast_model({'_id': fs_obj}, cls)
-    elif isinstance(fs_obj, dict):
-        if '_id' in fs_obj:
+def cast_model(obj, cls):
+    if isinstance(obj, ObjectId):
+        return cast_model({'_id': obj}, cls)
+    elif isinstance(obj, dict):
+        if '_id' in obj:
             # fetch and return updated version from server
-            db_fs = cast_model(cls.objects(pk=fs_obj['_id']).first(), cls)
-            pk = fs_obj['_id']
-            d1, d2 = dict(db_fs), dict(fs_obj)
+            db_fs = cast_model(cls.objects(pk=obj['_id']).first(), cls)
+            pk = obj['_id']
+            d1, d2 = dict(db_fs), dict(obj)
             d1.pop('_id', None), d2.pop('_id', None)
             return cls(pk=pk, **update_dic(d1, d2))
         else:
             # init
-            return cls(**fs_obj)
-    elif isinstance(fs_obj, cls):
-        return fs_obj
+            return cls(**obj)
+    elif isinstance(obj, cls):
+        return obj
     else:
         raise Exception('Unknown model format')
 
 
-def cast_dict(fs_obj, cls=None):
-    model = cast_model(fs_obj, cls=None)
+def cast_dict(obj, cls=None):
+    model = cast_model(obj, cls=None)
     return dict(model.to_mongo())
