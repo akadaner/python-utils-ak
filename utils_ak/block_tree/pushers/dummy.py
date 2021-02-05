25f8a807553ffb2546e210aae71bc2c6397946dc
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-02-05 15:31:44 +0300

    pushers: iterative_pushers init

--- utils_ak/block_tree/pushers/dummy.py
+++ utils_ak/block_tree/pushers/dummy.py
@@ -1,68 +1,17 @@
 import copy
 
-from utils_ak.clock import *
-from utils_ak import cast_dict_or_list
 from utils_ak.numeric import *
 from utils_ak.simple_vector import *
+
 
 from utils_ak.block_tree.parallelepiped_block import ParallelepipedBlock
 from utils_ak.block_tree.block import Block
 from utils_ak.block_tree.validation import validate_disjoint_by_axis
+from utils_ak.block_tree.pushers.pushers import simple_push
 
 from loguru import logger
 
-# todo: uncom debug
-
-def stack_push(parent, block):
-    axis = parent.props['axis']
-    cur_end = 0 if not parent.children else max(c.y[axis] - parent.x[axis] for c in parent.children)
-
-    x = block.props.get('x', cast_simple_vector(block.n_dims))
-    x[axis] = cur_end
-    block.props.update(x=x)
-    return add_push(parent, block)
-
-
-def test_stack_push():
-    logger.debug('Stack push test')
-    root = ParallelepipedBlock('root', n_dims=1, x=[2], axis=0)
-    a = ParallelepipedBlock('a', n_dims=1, size=[4], axis=0)
-    b = ParallelepipedBlock('b', n_dims=1, size=[3], axis=0)
-    stack_push(root, a)
-    stack_push(root, b)
-    logger.debug(root)
-
-
-@clockify()
-def simple_push(parent, block, validator=None, new_props=None):
-    block.set_parent(parent)
-
-    # update props for current try
-    new_props = new_props or {}
-    block.props.update(**new_props)
-    if validator:
-        try:
-            validator(parent, block)
-        except AssertionError as e:
-
-            try:
-                # reset parent
-                block.parent = None
-
-                # extract assertion message json
-                return cast_dict_or_list(e.__str__())  # {'disposition': 2}
-            except:
-                return {}
-    res = parent.add_child(block)
-    return res
-
-
-def add_push(parent, block, new_props=None):
-    return simple_push(parent, block, validator=None, new_props=new_props)
-
-
 def dummy_push(parent, block, validator, max_tries=24, start_from='last_end', iter_props=None):
-    clock('1')
     axis = parent.props['axis']
 
     if is_int_like(start_from):
@@ -85,7 +34,6 @@ def dummy_push(parent, block, validator, max_tries=24, start_from='last_end', it
     cur_x = cast_simple_vector(block.n_dims)
 
     cur_try = 0
-    clock('1')
 
     # logger.debug('Inserting blocks', parent=parent.props['cls'], block=block.props['cls'])
 
@@ -116,6 +64,9 @@ def dummy_push(parent, block, validator, max_tries=24, start_from='last_end', it
 
 
 def test_dummy_push():
+    from utils_ak.loguru import configure_loguru_stdout
+    configure_loguru_stdout()
+
     def brute_validator(parent, block):
         for c in parent.children:
             validate_disjoint_by_axis(c, block, axis=parent.props['axis'])
@@ -129,11 +80,5 @@ def test_dummy_push():
     logger.debug('Root', root=root)
 
 
-
-def push(parent, block, push_func=stack_push, **kwargs):
-    return push_func(parent, block, **kwargs)
-
-
 if __name__ == '__main__':
-    test_stack_push()
     test_dummy_push()
