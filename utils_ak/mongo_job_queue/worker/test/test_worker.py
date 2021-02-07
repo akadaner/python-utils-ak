fb2111d6ad2363c9f2566c1f3372019959c0dee3
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-02-07 20:43:02 +0300

    upd

--- utils_ak/mongo_job_queue/worker/test/test_worker.py
+++ utils_ak/mongo_job_queue/worker/test/test_worker.py
@@ -1,14 +1,9 @@
-import time
 import asyncio
-from loguru import logger
-from utils_ak.simple_microservice import run_listener_async
 from utils_ak.mongo_job_queue.worker.worker import Worker
+from utils_ak.mongo_job_queue.worker.worker_test import *
 
 
 class TestWorker(Worker):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     async def process(self):
         if self.payload.get('type') == 'batch':
             await asyncio.sleep(1)
@@ -28,41 +23,20 @@ class TestWorker(Worker):
         else:
             raise Exception(f'Bad payload type {self.payload.get("type")}')
 
+
 def test_batch():
-    from utils_ak.loguru import configure_loguru_stdout
-    configure_loguru_stdout('INFO')
-    run_listener_async('monitor', message_broker=('zmq', {'endpoints': {'monitor': {'endpoint': 'tcp://localhost:5555', 'type': 'sub'}}}))
-    time.sleep(2)
-    worker = TestWorker('worker_id', {'type': 'batch'}, message_broker=('zmq', {'endpoints': {'monitor': {'endpoint': 'tcp://localhost:5555', 'type': 'sub'}}}))
-    worker.run()
-    logger.info('Finished batch')
+    test_worker(TestWorker, {'type': 'batch'}, message_broker=('zmq', {'endpoints': {'monitor': {'endpoint': 'tcp://localhost:5555', 'type': 'sub'}}}))
 
 
 def test_streaming():
-    from utils_ak.loguru import configure_loguru_stdout
-    configure_loguru_stdout('INFO')
-    run_listener_async('monitor', message_broker=('zmq', {'endpoints': {'monitor': {'endpoint': 'tcp://localhost:5555', 'type': 'sub'}}}))
-    worker = TestWorker('worker_id', {'type': 'streaming'}, message_broker=('zmq', {'endpoints': {'monitor': {'endpoint': 'tcp://localhost:5555', 'type': 'sub'}}}))
-    worker.run()
+    test_worker(TestWorker, {'type': 'streaming'}, message_broker=('zmq', {'endpoints': {'monitor': {'endpoint': 'tcp://localhost:5555', 'type': 'sub'}}}))
 
 
 def test_deployment():
-    from utils_ak.loguru import configure_loguru_stdout
-    configure_loguru_stdout('DEBUG')
-    run_listener_async('monitor', message_broker=('zmq', {'endpoints': {'monitor': {'endpoint': 'tcp://localhost:5555', 'type': 'sub'}}}))
-
-    from utils_ak.deployment import DockerController
-    ctrl = DockerController()
-
-    import anyconfig
-    deployment = anyconfig.load('deployment.yml')
-    ctrl.stop(deployment['id'])
-    ctrl.start(deployment)
-    time.sleep(5)
-    ctrl.stop(deployment['id'])
+    test_worker_deployment('deployment.yml', message_broker=('zmq', {'endpoints': {'monitor': {'endpoint': 'tcp://localhost:5555', 'type': 'sub'}}}))
 
 
 if __name__ == '__main__':
-    test_batch()
+    # test_batch()
     # test_streaming()
-    # test_deployment()
+    test_deployment()
