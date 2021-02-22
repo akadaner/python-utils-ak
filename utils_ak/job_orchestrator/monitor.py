7fa4cff6cedf6e464cf36b5f7ce0b153c65721b6
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-02-22 12:30:17 +0300

    monitor hotfixes

--- utils_ak/job_orchestrator/monitor.py
+++ utils_ak/job_orchestrator/monitor.py
@@ -11,30 +11,26 @@ class MonitorActor:
         self.microservice.add_callback("monitor_in", "heartbeat", self._on_heartbeat)
         self.microservice.add_callback("monitor_in", "state", self._on_state)
 
-        # todo: del, hardcode
-        self.received_messages_cache = []
+        # # todo: del, hardcode
+        # self.received_messages_cache = []
 
     def _update_stalled(self):
-        for worker_id in self.workers:
-            if "status" not in self.workers[worker_id]:
+        for worker_id, worker in self.workers.items():
+            if "status" not in worker:
                 # non-initialized
-                # todo: make properly
                 continue
 
-            if self.workers[worker_id]["status"] == "success":
+            if worker["status"] == "success":
                 continue
 
             if (
-                "last_heartbeat" in self.workers[worker_id]
-                and (
-                    datetime.utcnow() - self.workers[worker_id]["last_heartbeat"]
-                ).total_seconds()
+                "last_heartbeat" in worker
+                and (datetime.utcnow() - worker["last_heartbeat"]).total_seconds()
                 > self.heartbeat_timeout
             ):
                 self._update_status(worker_id, "stalled")
 
     def _on_heartbeat(self, topic, id):
-        # todo: make properly
         if id in self.workers:
             self.workers[id]["last_heartbeat"] = datetime.utcnow()
 
@@ -52,18 +48,18 @@ class MonitorActor:
             self.workers[worker_id]["status"] = status
 
     def _on_state(self, topic, id, status, state):
-        # todo: del, preview hardcode
-        if id is not None and state is not None:
-            key = id + state
-            if key not in self.received_messages_cache:
-                self.received_messages_cache.append(key)
-            else:
-                return
+        # # todo: del, preview hardcode
+        # if id is not None and state is not None:
+        #     key = id + state
+        #     if key not in self.received_messages_cache:
+        #         self.received_messages_cache.append(key)
+        #     else:
+        #         return
 
         if id not in self.workers:
             self.microservice.publish("monitor_out", "new", {"id": id})
             self.workers[id] = {}
-            self._update_status(id, status)
-            self.workers[id]["state"] = state
+        self._update_status(id, status)
+        self.workers[id]["state"] = state
 
         self.microservice.logger.info("Current monitor state", state=self.workers)
