bd3d5582b18737d6c851bf989df9a1a6a6fa3fa4
Arsenii Kadaner <arseniikadaner@gmail.com>
2021-03-16 10:11:00 +0300

    cases

--- utils_ak/naming/normalize.py
+++ utils_ak/naming/normalize.py
@@ -1,4 +1,4 @@
-def to_lower_name(upper_case_name):
+def to_lower_case(upper_case_name):
     res = ""
     for l in upper_case_name:
         if not res:
@@ -16,6 +16,7 @@ def to_lower_name(upper_case_name):
 
 
 def to_upper_case(lower_case_name, camel=False):
+    assert all(not c.isupper() for c in lower_case_name)
     values = lower_case_name.split("_")
     values = [v.title() for v in values]
     if camel:
@@ -24,8 +25,18 @@ def to_upper_case(lower_case_name, camel=False):
     return "".join(values)
 
 
+def test():
+    assert to_lower_case("camelCase") == "camel_case"
+    assert to_lower_case("UpperCase") == "upper_case"
+    assert to_lower_case("upper_case") == "upper_case"
+    assert to_upper_case("upper_case") == "UpperCase"
+    assert to_upper_case("upper_case", camel=True) == "upperCase"
+
+    try:
+        to_upper_case("UpperCase") == "UpperCase"
+    except AssertionError:
+        print("Wrong input")
+
+
 if __name__ == "__main__":
-    print(to_lower_name("camelCase"))  # camel_case
-    print(to_lower_name("UpperCase"))  # upper_case
-    print(to_upper_case("upper_case"))  # UpperCase
-    print(to_upper_case("upper_case", camel=True))  # upperCase
+    test()
