8af3a996f09aafb8485b1b2e6dc987857b6791ba
marklidenberg <marklidenberg@gmail.com>
2021-09-15 11:16:11 +0300

    validate_order_by_axis

--- utils_ak/block_tree/validation.py
+++ utils_ak/block_tree/validation.py
@@ -19,6 +19,19 @@ def validate_disjoint_by_axis(b1, b2, axis=0, distance=0, ordered=False):
     )
 
 
+def validate_order_by_axis(b1, b2, axis=0, equal_allowed=False):
+    i1_single = (
+        (b1.x[axis] - 1, b1.x[axis]) if equal_allowed else (b1.x[axis], b1.x[axis] + 1)
+    )  # if equal allowed - start on one position earlier
+    i2_single = (b2.x[axis], b2.x[axis] + 1)
+    validate_disjoint_by_intervals(
+        i1_single,
+        i2_single,
+        distance=0,
+        ordered=True,
+    )
+
+
 def _calc_interval_intersection(i1, i2):
     return max(min(i1[1], i2[1]) - max(i1[0], i2[0]), 0)
 
