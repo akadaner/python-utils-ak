f7c596ecdfad639b089c84216956ae908a143c3c
marklidenberg <marklidenberg@gmail.com>
2022-06-28 11:27:07 +0400

    init

--- src/python/libs/lessmore.clock/lessmore/clock/clock.py
+++ src/python/libs/lessmore.clock/lessmore/clock/clock.py
@@ -2,11 +2,7 @@ import time
 import pandas as pd
 import json
 
-
-# todo: create subclock - for prefixes
-# todo: remove pandas dependency
-# todo: auto trie generation? Generate subtree while possible
-# todo: remove overhead from timing. Is it even possible?
+from lessmore.code_block import code
 
 
 class Clock:
@@ -115,9 +111,7 @@ class Clock:
         keys = list(self.laps.keys())
         if not keys:
             return pd.DataFrame(index=["count", "mean", "sum", "max", "min"])
-        df = pd.concat(
-            [pd.DataFrame({k: self.laps[k]}) for k in keys], ignore_index=True, axis=1
-        )
+        df = pd.concat([pd.DataFrame({k: self.laps[k]}) for k in keys], ignore_index=True, axis=1)
         df.columns = keys
         stats_df = df.agg(["count", "mean", "sum", "max", "min"])
         return stats_df
@@ -238,102 +232,3 @@ def clockify(key=None):
         return inner
 
     return _clockify
-
-
-@clockify("time")
-def test_time():
-    time.sleep(1)
-
-
-@clockify("time_with_exception")
-def test_time_with_exception():
-    time.sleep(1)
-    raise Exception("test")
-
-
-# todo: make a reproducible test
-def test():
-    # usage 1
-    clock = Clock()
-
-    test_time()
-    try:
-        test_time_with_exception()
-    except:
-        pass
-    print(clock.stats())
-    clock.reset()
-
-    # start clock
-    clock.clock("first")
-    time.sleep(0.1)
-    # stop clock
-    clock.clock("first")
-
-    clock.clock("second")
-    time.sleep(0.2)
-    clock.clock("second")
-
-    # this will add statistics to the 'first' key
-    clock.clock("first")
-    time.sleep(0.1)
-    clock.clock("first")
-
-    print(clock.stats())
-
-    # usage 2
-    clock = Clock(auto_close=True)
-    clock.clock("first")
-    time.sleep(0.1)
-    # this will auto-close 'first' clock
-    clock.clock("second")
-    time.sleep(0.2)
-
-    # this stops all clocks
-    clock.stop()
-    print("Auto close")
-    print(clock.stats())
-
-    clock = Clock()
-    # trie example
-    clock.reset()
-    clock.clock("a")
-
-    for i in range(10):
-        clock.clock("a/b")
-        time.sleep(0.1)
-        clock.clock("a/b")
-    clock.clock("a")
-    print(clock.stats())
-
-    try:
-        clock.print_trie()
-        clock.print_trie(by="count")
-    except:
-        pass
-
-    print(clock["a"])
-
-    clock.save("clock.json")
-
-    clock.reset()
-    print(clock.stats())
-    clock.load("clock.json")
-    print(clock.stats())
-
-    # check max observations
-    clock = Clock(max_observations=3)
-    for i in range(10):
-        print(clock.laps.get("a"))
-        clock.start("a")
-        time.sleep(0.01 * i)
-        clock.stop("a")
-
-    with clock("asdf"):
-        time.sleep(1)
-
-    print(clock.stats())
-
-
-if __name__ == "__main__":
-    test()
